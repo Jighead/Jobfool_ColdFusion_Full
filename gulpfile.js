d8e717cb4cc3c828785819c014c5c208452f3924
Jighead <gerdsuhr@gmail.com>
2016-05-07 22:09:04 -0400

    latest

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var obfuscate = require('gulp-obfuscate');
 var concat = require('gulp-concat');
 var uncss = require('gulp-uncss');
+var csso = require('gulp-csso');
 var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 
 gulp.task('uncss', function() {
   var csssource = ['assets/css/app.css'];
@@ -17,14 +23,53 @@ gulp.task('uncss', function() {
         }))
   //.pipe(cleanCSS())
   .pipe(gulp.dest('dist/assets/css/'));
-});  
+});
+
+gulp.task('obfuscate', function() {
+  return gulp.src('assets/js/unify-app.js')
+    .pipe(obfuscate({ replaceMethod: obfuscate.ZALGO }))
+    .pipe(gulp.dest('dist/assets/js/'));
+});
+
+gulp.task('uglify', function() {
+  return gulp.src('assets/js/unify-app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js/'));
+});
+
+gulp.task('csso-app', function() {
+  return gulp.src('assets/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(csso())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/assets/css/'));
+});
+
+gulp.task('csso-plugins', function() {
+  return gulp.src('assets/plugins/**/css/*.css')
+    //.pipe(sourcemaps.init())
+    .pipe(csso())
+    //.pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/assets/plugins/**/css/'));
+});
 
 gulp.task('concatCSS', function() {
   return gulp.src('dist/assets/css/*.css')
     //.pipe(concat('app.css'))
     .pipe(gulp.dest('dist/assets/css/'));
 });
-        
+      
+gulp.task('images', function(){
+  return gulp.src('assets/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache (imagemin()) )
+  .pipe(gulp.dest('dist/assets/images'))
+});
+              
+gulp.task('img', function(){
+  return gulp.src('assets/img/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache (imagemin()) )
+  .pipe(gulp.dest('dist/assets/img'))
+});
 
 gulp.task('copy', function() {
   // copy any html files in source/ to public/
@@ -48,11 +93,11 @@ var rootfiles = ['addalert.cfm'
     gulp.src('privacy/*.*').pipe(gulp.dest('dist/privacy/'));
     
     
-    gulp.src('assets/css/app.css').pipe(gulp.dest('dist/assets/css/'));
+    //gulp.src('assets/css/app.css').pipe(gulp.dest('dist/assets/css/'));
     //gulp.src('assets/css/u-dark-blue.css').pipe(gulp.dest('dist/assets/css/'));
     gulp.src('assets/css/grid-ms.css').pipe(gulp.dest('dist/assets/css/'));
-    gulp.src('assets/img/**/*.*').pipe(gulp.dest('dist/assets/img/'));
-    gulp.src('assets/images/**/*.*').pipe(gulp.dest('dist/assets/images/'));
+    //gulp.src('assets/img/**/*.*').pipe(gulp.dest('dist/assets/img/'));
+    //gulp.src('assets/images/**/*.*').pipe(gulp.dest('dist/assets/images/'));
     
     
     gulp.src('assets/plugins/bootstrap/**/*.*').pipe(gulp.dest('dist/assets/plugins/bootstrap/'));
@@ -75,5 +120,5 @@ var rootfiles = ['addalert.cfm'
  return
 });
            
-gulp.task('default',[ 'copy', 'uncss' ]);
+gulp.task('default',[ 'copy', 'csso-app','images', 'img' ]);
 
